851e9e4cc411bf8676cfe8cd609e5b785ee12eb8
Jannes Behrens <jannescb@me.com>
2021-08-13 13:07:14 +0200

    update app.ts

--- publish/resources/js/app.ts
+++ publish/resources/js/app.ts
@@ -1,40 +1,19 @@
-import { createApp, h } from 'vue';
-import {
-    App as InertiaApp,
-    plugin as InertiaPlugin,
-} from '@inertiajs/inertia-vue3';
+import { createApp, h } from 'vue'
+import { createInertiaApp } from '@inertiajs/inertia-vue3'
 import DefaultLayout from './Layouts/Default.vue';
 import Components from './components'
 
-const el = document.getElementById('app');
 
-if (el instanceof HTMLElement) {
-    /**
-     * Create a new Inertia App
-     * 
-     */
-    const app = createApp({
-        render: () =>
-            h(InertiaApp, {
-                initialPage: JSON.parse(el.dataset.page as string),
-                resolveComponent: (name) => {
-                    const page = require(`./Pages/${name}`).default
-                    page.layout = page.layout || DefaultLayout
-                    return page
-                },
-            }),
-    });
-    
-    /**
-     * Load Plugins
-     * 
-     */
-    app.use(InertiaPlugin);
-    app.use(Components);
-    
-    /**
-     * Mount the app in the app container
-     * 
-     */
-    app.mount(el);
-}
+createInertiaApp({
+    resolve: name => {
+        const page = require(`./Pages/${name}`).default
+        page.layout = page.layout || DefaultLayout
+        return page
+    },
+    setup({ el, app, props, plugin }) {
+        createApp({ render: () => h(app, props) })
+            .use(plugin)
+            .use(Components)
+            .mount(el)
+    },
+})
