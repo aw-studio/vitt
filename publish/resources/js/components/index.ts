d351d15282dcfb4d6e87a9a157db0cbeccda3797
Lennart Carstens-Behrens <lennart.carbe@gmail.com>
2021-09-07 10:39:08 +0200

    added typescript types

--- publish/resources/js/components/index.ts
+++ publish/resources/js/components/index.ts
@@ -1,17 +1,19 @@
+import { Plugin } from 'vue';
 import Button from './Ui/Button.vue';
 import Head from './App/Head.vue';
 
 const plugin = {
-    install(app: any) {
+    install(app) {
         /**
          * UI-Components
          */
         app.component('UiButton', Button);
+
         /**
          * App-Components
          */
         app.component('AppHead', Head);
     },
-};
+} as Plugin;
 
 export default plugin ;
